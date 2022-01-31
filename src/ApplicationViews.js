ab4b3e95767897153a8e8037728a3cbc8832143d
Jake Lashenik <jmlashen@gmail.com>
2022-01-30 23:38:22 -0600

    testing nav bar and routing

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -4,13 +4,16 @@ import { DrillList } from "./components/drills/DrillList";
 import { MyStopwatch } from "./Timer";
 import { Home } from "./Home";
 import { Contacts } from "./Contact";
+import { NavBarZ } from "./components/nav/NavBarZ";
 
 export const ApplicationViews = () => {
 
     return (
 
         <>
-
+    <Route>
+        <NavBarZ />
+    </Route>
 
             <Route exact path="/drills">
                 <DrillList />
